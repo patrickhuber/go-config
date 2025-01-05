21a56c3d1760f218c2853f7f8a41975918583f4b
Patrick Huber <182398+patrickhuber@users.noreply.github.com>
2025-01-05 11:44:39 -0500

    adds yaml

--- yaml.go
+++ yaml.go
@@ -1,1 +1,27 @@
 package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type YamlProvider struct {
+	file string
+}
+
+func NewYaml(file string) *YamlProvider {
+	return &YamlProvider{
+		file: file,
+	}
+}
+
+func (p *YamlProvider) Get() (any, error) {
+	buf, err := os.ReadFile(p.file)
+	if err != nil {
+		return nil, err
+	}
+	var data any
+	err = yaml.Unmarshal(buf, &data)
+	return data, err
+}
