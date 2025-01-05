1a9ddf44176a299f39caca2ce46f749c75d54401
Patrick Huber <182398+patrickhuber@users.noreply.github.com>
2025-01-05 13:48:44 -0500

    adds flag source

---
+++ flag.go
@@ -0,0 +1,89 @@
+package config
+
+import (
+	"flag"
+	"fmt"
+)
+
+type Flag interface {
+	Set(value string) error
+	String() string
+	Value() any
+}
+
+type StringFlag struct {
+	Name    string
+	Default string
+	Usage   string
+	value   string
+}
+
+func (s *StringFlag) Set(value string) error {
+	s.value = value
+	return nil
+}
+
+func (s *StringFlag) String() string {
+	return s.value
+}
+
+func (s *StringFlag) Value() any {
+	return s.value
+}
+
+type StringSliceFlag struct {
+	Name    string
+	Default []string
+	Usage   string
+	value   []any
+}
+
+func (s *StringSliceFlag) Set(value string) error {
+	s.value = append(s.value, value)
+	return nil
+}
+
+func (s *StringSliceFlag) String() string {
+	return fmt.Sprintf("%v", s.value)
+}
+
+func (s *StringSliceFlag) Value() any {
+	return s.value
+}
+
+type FlagProvider struct {
+	flags []Flag
+	args  []string
+}
+
+func NewFlag(flags []Flag, args []string) *FlagProvider {
+	return &FlagProvider{
+		flags: flags,
+		args:  args,
+	}
+}
+
+func (p *FlagProvider) Get() (any, error) {
+	m := map[string]any{}
+	flagset := flag.NewFlagSet("any", flag.ContinueOnError)
+	for _, f := range p.flags {
+		switch ty := f.(type) {
+		case *StringFlag:
+			flagset.Var(ty, ty.Name, ty.Usage)
+		case *StringSliceFlag:
+			flagset.Var(ty, ty.Name, ty.Usage)
+		}
+	}
+	err := flagset.Parse(p.args)
+	if err != nil {
+		return nil, err
+	}
+	flagset.Visit(func(f *flag.Flag) {
+		flagValue, ok := f.Value.(Flag)
+		if !ok {
+			return
+		}
+		m[f.Name] = flagValue.Value()
+	})
+	return m, nil
+}
---
+++ flag_test.go
@@ -0,0 +1,44 @@
+package config_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/patrickhuber/go-config"
+)
+
+func TestFlag(t *testing.T) {
+	type test struct {
+		name     string
+		args     []string
+		flags    []config.Flag
+		expected map[string]any
+	}
+
+	tests := []test{
+		{
+			name:     "string",
+			args:     []string{"--test", "abc"},
+			flags:    []config.Flag{&config.StringFlag{Name: "test", Default: "", Usage: "uses the test"}},
+			expected: map[string]any{"test": "abc"},
+		},
+		{
+			name:     "repeat",
+			args:     []string{"--test", "abc", "--test", "123"},
+			flags:    []config.Flag{&config.StringSliceFlag{Name: "test", Default: nil, Usage: "uses the test"}},
+			expected: map[string]any{"test": []any{"abc", "123"}},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			p := config.NewFlag(test.flags, test.args)
+			cfg, err := p.Get()
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !reflect.DeepEqual(cfg, test.expected) {
+				t.Fatalf("expected configurations to be equal")
+			}
+		})
+	}
+}
