5133097c00cdd6736a0956b16d775fd5e24885f4
patrickhuber <patrickhuber@users.noreply.github.com>
2025-02-15 17:04:57 -0500

    working with point delete and point add

---
+++ diff.go
@@ -0,0 +1,186 @@
+package config
+
+import (
+	"fmt"
+	"maps"
+	"strconv"
+)
+
+type ChangeType interface {
+	changeType()
+}
+
+type changeType string
+
+func (changeType) changeType() {}
+
+const (
+	Create changeType = "create"
+	Update changeType = "update"
+	Delete changeType = "delete"
+)
+
+type Change struct {
+	ChangeType ChangeType
+	Path       []string
+	From       any
+	To         any
+}
+
+func Diff(from, to any) ([]Change, error) {
+	return diff(from, to, []string{})
+}
+
+func diff(from, to any, path []string) ([]Change, error) {
+	if from == nil && to != nil {
+		return []Change{
+			{
+				ChangeType: Create,
+				Path:       path,
+				From:       from,
+				To:         to,
+			},
+		}, nil
+	}
+	if from != nil && to == nil {
+		return []Change{
+			{
+				ChangeType: Delete,
+				Path:       path,
+				From:       from,
+				To:         to,
+			},
+		}, nil
+	}
+	switch f := from.(type) {
+	case string:
+		return diffOf(f, to, path), nil
+	case float64:
+		return diffOf(f, to, path), nil
+	case bool:
+		return diffOf(f, to, path), nil
+	case map[string]any:
+		return mapDiff(f, to, path)
+	case []any:
+		return sliceDiff(f, to, path)
+	}
+	return nil, fmt.Errorf("unrecognized source type %T. expected string, float64, bool, map[string]any or []any", from)
+}
+
+func diffOf[T comparable](from T, to any, path []string) []Change {
+	t, isTypeT := to.(T)
+	if !isTypeT && t == from {
+		return nil
+	}
+	return []Change{
+		{
+			ChangeType: Update,
+			Path:       path,
+			From:       from,
+			To:         to,
+		},
+	}
+}
+
+func mapDiff(fromMap map[string]any, to any, path []string) ([]Change, error) {
+	toMap, toIsMap := to.(map[string]any)
+	if !toIsMap {
+		return []Change{
+			{
+				ChangeType: Update,
+				Path:       path,
+				From:       fromMap,
+				To:         to,
+			},
+		}, nil
+	}
+	var changes []Change
+
+	// process deletions and updates
+	for fromKey := range maps.Keys(fromMap) {
+		value, exists := toMap[fromKey]
+		if !exists {
+			value = nil
+		}
+		recurseChanges, err := diff(fromMap[fromKey], value, append(path, fromKey))
+		if err != nil {
+			return nil, err
+		}
+		changes = append(changes, recurseChanges...)
+	}
+	// process additions
+	for toKey := range maps.Keys(toMap) {
+		_, exists := fromMap[toKey]
+		if exists {
+			continue
+		}
+		recurseChanges, err := diff(nil, toMap[toKey], append(path, toKey))
+		if err != nil {
+			return nil, err
+		}
+		changes = append(changes, recurseChanges...)
+	}
+	return changes, nil
+}
+
+func sliceDiff(fromSlice []any, to any, path []string) ([]Change, error) {
+	toSlice, toIsSlice := to.([]any)
+	if !toIsSlice {
+		return []Change{
+			{
+				ChangeType: Update,
+				Path:       path,
+				From:       fromSlice,
+				To:         toSlice,
+			},
+		}, nil
+	}
+
+	var changes []Change
+
+	fromMap := createReverseLookup(fromSlice)
+	for toIndex, toValue := range toSlice {
+		// was the toValue added?
+		_, ok := fromMap[toValue]
+		if ok {
+			continue
+		}
+		changes = append(changes, Change{
+			ChangeType: Create,
+			Path:       append(path, strconv.Itoa(toIndex)),
+			From:       nil,
+			To:         toValue,
+		})
+	}
+
+	toMap := createReverseLookup(toSlice)
+	for fromIndex, fromValue := range fromSlice {
+		// was the fromValue removed?
+		_, ok := toMap[fromValue]
+		if ok {
+			continue
+		}
+		changes = append(changes, Change{
+			ChangeType: Delete,
+			Path:       append(path, strconv.Itoa(fromIndex)),
+			From:       fromValue,
+			To:         nil,
+		})
+	}
+
+	return changes, nil
+}
+
+func createReverseLookup(slice []any) map[any][]int {
+	m := make(map[any][]int)
+	for index, value := range slice {
+		v, ok := m[index]
+		if !ok {
+			m[value] = []int{index}
+		} else {
+			v = append(v, index)
+			m[value] = v
+		}
+	}
+	return m
+}
---
+++ diff_test.go
@@ -0,0 +1,246 @@
+package config_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/patrickhuber/go-config"
+)
+
+func TestDiff(t *testing.T) {
+	type test struct {
+		name     string
+		from     any
+		to       any
+		expected []config.Change
+	}
+	tests := []test{
+		{
+			name: "string_update",
+			from: "hello",
+			to:   "world",
+			expected: []config.Change{
+				{
+					ChangeType: config.Update,
+					From:       "hello",
+					To:         "world",
+				},
+			},
+		},
+		{
+			name: "float_update",
+			from: float64(1),
+			to:   float64(2),
+			expected: []config.Change{
+				{
+					ChangeType: config.Update,
+					From:       float64(1),
+					To:         float64(2),
+				},
+			},
+		},
+		{
+			name: "bool_update",
+			from: true,
+			to:   false,
+			expected: []config.Change{
+				{
+					ChangeType: config.Update,
+					From:       true,
+					To:         false,
+				},
+			},
+		},
+		{
+			name: "map_delete",
+			from: map[string]any{"hello": "world"},
+			to:   map[string]any{},
+			expected: []config.Change{
+				{
+					Path:       []string{"hello"},
+					ChangeType: config.Delete,
+					From:       "world",
+					To:         nil,
+				},
+			},
+		},
+		{
+			name: "map_create",
+			from: map[string]any{},
+			to:   map[string]any{"hello": "world"},
+			expected: []config.Change{
+				{
+					Path:       []string{"hello"},
+					ChangeType: config.Create,
+					From:       nil,
+					To:         "world",
+				},
+			},
+		},
+		{
+			name: "map_update",
+			from: map[string]any{"hello": "world"},
+			to:   map[string]any{"hello": "config"},
+			expected: []config.Change{
+				{
+					Path:       []string{"hello"},
+					ChangeType: config.Update,
+					From:       "world",
+					To:         "config",
+				},
+			},
+		},
+		{
+			name: "map_and_not_map",
+			from: map[string]any{"hello": "world"},
+			to:   "string",
+			expected: []config.Change{
+				{
+					Path:       nil,
+					ChangeType: config.Update,
+					From:       map[string]any{"hello": "world"},
+					To:         "string",
+				},
+			},
+		},
+		{
+			name: "not_map_and_map",
+			from: "string",
+			to:   map[string]any{"hello": "world"},
+			expected: []config.Change{
+				{
+					Path:       nil,
+					ChangeType: config.Update,
+					From:       "string",
+					To:         map[string]any{"hello": "world"},
+				},
+			},
+		},
+		{
+			name: "slice_delete_0",
+			from: []any{"hello", "world"},
+			to:   []any{"world"},
+			expected: []config.Change{
+				{
+					Path:       []string{"0"},
+					ChangeType: config.Delete,
+					From:       "hello",
+					To:         nil,
+				},
+			},
+		},
+		{
+			name: "slice_create_start",
+			from: []any{"world"},
+			to:   []any{"hello", "world"},
+			expected: []config.Change{
+				{
+					Path:       []string{"0"},
+					ChangeType: config.Create,
+					From:       nil,
+					To:         "hello",
+				},
+			},
+		},
+		{
+			name: "slice_create_end",
+			from: []any{"hello"},
+			to:   []any{"hello", "world"},
+			expected: []config.Change{
+				{
+					Path:       []string{"1"},
+					ChangeType: config.Create,
+					From:       nil,
+					To:         "world",
+				},
+			},
+		},
+		{
+			name: "slice_create_insert",
+			from: []any{"hello", "world"},
+			to:   []any{"hello", "new", "world"},
+			expected: []config.Change{
+				{
+					Path:       []string{"1"},
+					ChangeType: config.Create,
+					From:       nil,
+					To:         "new",
+				},
+			},
+		},
+		{
+			name: "slice_delete_middle",
+			from: []any{"hello", "new", "world"},
+			to:   []any{"hello", "world"},
+			expected: []config.Change{
+				{
+					Path:       []string{"1"},
+					ChangeType: config.Delete,
+					From:       "new",
+					To:         nil,
+				},
+			},
+		},
+		{
+			name: "slice_reverse",
+			from: []any{"hello", "new", "world"},
+			to:   []any{"world", "new", "hello"},
+			expected: []config.Change{
+				{
+					Path:       []string{"0"},
+					ChangeType: config.Update,
+					From:       "hello",
+					To:         "world",
+				},
+				{
+					Path:       []string{"2"},
+					ChangeType: config.Update,
+					From:       "world",
+					To:         "hello",
+				},
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual, err := config.Diff(test.from, test.to)
+			if err != nil {
+				t.Fatal(err)
+			}
+			err = assertChangesEqual(test.expected, actual)
+			if err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
+
+func assertChangesEqual(expected, actual []config.Change) error {
+	if len(actual) != len(expected) {
+		return fmt.Errorf("expected length %d actual length %d", len(expected), len(actual))
+	}
+	for i := range expected {
+		err := assertChangeEqual(expected[i], actual[i])
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func assertChangeEqual(expected, actual config.Change) error {
+	if expected.ChangeType != actual.ChangeType {
+		return fmt.Errorf("expected change type %s but found change type %s", expected.ChangeType, actual.ChangeType)
+	}
+	if len(expected.Path) != len(actual.Path) {
+		return fmt.Errorf("expected path length to be %d but found %d", len(expected.Path), len(actual.Path))
+	}
+	if !reflect.DeepEqual(expected.From, actual.From) {
+		return fmt.Errorf("expected To to be %v but found %v", expected.From, actual.From)
+	}
+	if !reflect.DeepEqual(expected.To, actual.To) {
+		return fmt.Errorf("expected From to be %v but found %v", expected.To, actual.To)
+	}
+	return nil
+}
