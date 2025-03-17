2ecf457dcb21ebb1a35073e820e0dc1664d0455d
patrickhuber <patrickhuber@users.noreply.github.com>
2025-03-17 14:04:38 -0400

    adds memory provider

---
+++ memory.go
@@ -0,0 +1,15 @@
+package config
+
+type MemoryProvider struct {
+	memory map[string]any
+}
+
+func NewMemory(memory map[string]any) *MemoryProvider {
+	return &MemoryProvider{
+		memory: memory,
+	}
+}
+
+func (m *MemoryProvider) Get() (any, error) {
+	return m.memory, nil
+}
---
+++ memory_test.go
@@ -0,0 +1,26 @@
+package config_test
+
+import (
+	"testing"
+
+	"github.com/patrickhuber/go-config"
+)
+
+func TestMemory(t *testing.T) {
+	m := config.NewMemory(map[string]any{"hello": "world"})
+	value, err := m.Get()
+	if err != nil {
+		t.Fatal(err)
+	}
+	valueMap, ok := value.(map[string]any)
+	if !ok {
+		t.Fatalf("expected map but found %T", value)
+	}
+	helloValue, ok := valueMap["hello"]
+	if !ok {
+		t.Fatal("missing key 'hello'")
+	}
+	if helloValue != "world" {
+		t.Fatalf("expected 'world' found %s", helloValue)
+	}
+}
