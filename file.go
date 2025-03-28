45c7c3526651f8f05d3e8363837d51180d4d02d7
patrickhuber <patrickhuber@users.noreply.github.com>
2025-03-28 09:31:54 -0400

    moves to option list

--- file.go
+++ file.go
@@ -5,27 +5,41 @@ import (
 )
 
 type fileProvider struct {
-	file   string
-	codec  Codec
-	option FileOption
+	file    string
+	codec   Codec
+	options []FileOption
 }
+
+type FileBeforeGet func(ctx *GetContext, provider Provider, file string) (any, error)
 
 type FileOption struct {
 	Transformers []Transformer
+	BeforeGet    FileBeforeGet
 }
 
 func NewFile(file string, codec Codec, options ...FileOption) Provider {
 	provider := &fileProvider{
-		file:  file,
-		codec: codec,
-	}
-	for _, option := range options {
-		provider.option.Transformers = append(provider.option.Transformers, option.Transformers...)
+		file:    file,
+		codec:   codec,
+		options: options,
 	}
 	return provider
 }
 
 func (provider *fileProvider) Get(ctx *GetContext) (any, error) {
+	for _, option := range provider.options {
+		if option.BeforeGet == nil {
+			continue
+		}
+		beforeGet, err := option.BeforeGet(ctx, provider, provider.file)
+		if err != nil {
+			return nil, err
+		}
+		ctx.MergedConfiguration, err = Merge(ctx.MergedConfiguration, beforeGet)
+		if err != nil {
+			return nil, err
+		}
+	}
 	buf, err := os.ReadFile(provider.file)
 	if err != nil {
 		return nil, err
@@ -35,7 +49,11 @@ func (provider *fileProvider) Get(ctx *GetContext) (any, error) {
 	if err != nil {
 		return nil, err
 	}
-	return transform(data, provider.option.Transformers)
+	var transformers []Transformer
+	for _, option := range provider.options {
+		transformers = append(transformers, option.Transformers...)
+	}
+	return transform(data, transformers)
 }
 
 func (provider *fileProvider) Set(ctx *SetContext, value any) error {
